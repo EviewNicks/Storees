ce6a263c59d2c5857265ded3b316178fddf18957
Ardiansyah <eviewnicks@gmail.com>
2025-01-15 21:06:27 +0800

    favorite Page

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import SectionTitle from "@/components/global/SectionTitle";
+import ProductsGrid from "@/components/products/ProductsGrid";
+import { fetchUserFavorites } from "@/utils/action";
 
-const favoritesPage = () => {
-  return <div>favoritesPage</div>;
+const favoritesPage = async () => {
+  const favorites = await fetchUserFavorites();
+  if (favorites.length === 0)
+    return <SectionTitle text="You ave no favorites yet." />;
+
+  return (
+    <div>
+      <SectionTitle text="Product List" />
+      <ProductsGrid products={favorites.map((favorite) => favorite.product)} />
+    </div>
+  );
 };
 
 export default favoritesPage;
