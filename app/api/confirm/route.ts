03bcc8bdf49974f734f7a303cfc73cb164af24a4
Ardiansyah <eviewnicks@gmail.com>
2025-01-17 07:50:08 +0800

    Chance Database to MongoDB

--- app/api/confirm/route.ts
+++ app/api/confirm/route.ts
@@ -1,13 +1,13 @@
-import Stripe from 'stripe';
+import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { type NextRequest } from 'next/server';
-import db from '@/utils/db';
+import { type NextRequest } from "next/server";
+import db from "@/utils/db";
 
 export const GET = async (req: NextRequest) => {
   const { searchParams } = new URL(req.url);
-  const session_id = searchParams.get('session_id') as string;
+  const session_id = searchParams.get("session_id") as string;
 
   try {
     const session = await stripe.checkout.sessions.retrieve(session_id);
@@ -15,7 +15,7 @@ export const GET = async (req: NextRequest) => {
 
     const orderId = session.metadata?.orderId;
     const cartId = session.metadata?.cartId;
-    if (session.status === 'complete') {
+    if (session.status === "complete") {
       await db.order.update({
         where: {
           id: orderId,
@@ -34,8 +34,8 @@ export const GET = async (req: NextRequest) => {
     console.log(err);
     return Response.json(null, {
       status: 500,
-      statusText: 'Internal Server Error',
+      statusText: "Internal Server Error",
     });
   }
-  redirect('/orders');
+  redirect("/orders");
 };
